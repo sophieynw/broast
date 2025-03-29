68bbc48a37390cf793076af539bef73e83d37930
Sofiia Tsepotan <zepotan.sonja@gmail.com>
2025-03-29 19:09:52 -0400

    added a bear component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BearComponent } from './bear/bear.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BearComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
